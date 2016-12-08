d385a4fb889c88d720b77445f4e00e5097998927
Unknown <d00169143@student.dkit.ie>
2016-12-08 12:35:55 +0000

    Working still on searchBySubject

--- emailClientSystem/emailClientSystem/Main.cpp
+++ emailClientSystem/emailClientSystem/Main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "User.h"
 #include "Email.h"
-using namespace std;
 
-void searchEmailUsingSubject(string userInput);
 
 int main()
 {
@@ -12,16 +10,9 @@ int main()
 
 	testEmail.print();
 
+	testUser.searchEmailUsingSubject("Paddy");
+
 	system("pause");
 	return 0;
 }
 
-//Search EMAIL By Subject
-void searchEmailUsingSubject(string userInput)
-{
-	//get All subjects
-	//Email::getSubject();
-	//loop through subjects 
-	//match subject
-	//return 
-}
